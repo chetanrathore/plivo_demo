fe5506b185c48d1d59d1801a82c8a3cc091028f2
Esha Singh <singh.esha.dev@gmail.com>
2017-03-04 11:34:15 +0530

    XML generate when call fired.

--- config/config.js
+++ config/config.js
@@ -3,6 +3,6 @@ module.exports = {
   jwtExpiresIn: '24y',
   authId: 'MAZWI1OGZIYZBKYTVHMG',
   authToken: 'MWMyNzM1Y2RlMjY1MWQ0OTMzZWJjY2JmYTc5MTI1',
-  tmpServer: 'https://61caab23.ngrok.io',
+  tmpServer: 'http://81ada20a.ngrok.io',
   webPort: 3000
 };
